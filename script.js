4b6de45bda8ff297121fbb175dbbc56c37d33717
ciaranpk <ciaranpaik@gmail.com>
2024-08-09 10:39:14 -0400

    hello

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 console.log('Hello, World! can of beans');
-alert("Hello, World! Alert");
-document.write('Hello, World! Document');
+alert("Hello, World! how are you");
+document.write('Hello, World! to the game');
